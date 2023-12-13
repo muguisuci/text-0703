019b8cee8d2641cf6f2eedba4cb4c5a9650893f0
muguisuci <3515851731@qq.com>
2023-12-13 16:07:02 +0800

    sentry测试

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,8 @@ module.exports = defineConfig({
     devtool: "source-map",
     plugins: [
       sentryWebpackPlugin({
-        org: process.env.SENTRY_ORG,
-        project: process.env.SENTRY_PROJECT,
+        org: HOPU,
+        project: vue-search,
         authToken:"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"
       }),
       
