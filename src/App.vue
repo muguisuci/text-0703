681c577e10c8b38394fcd5aa7706fc98c9f35b2f
muguisuci <3515851731@qq.com>
2023-12-13 16:25:56 +0800

    sentry测试

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div @click="fn">jdhfak</div>
+    <button @click="fn()">jdhfak</button>
   </div>
 </template>
 
